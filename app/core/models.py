483ea43bc7ba4337056ab1a60a9aa8b672257a0c
minna123-cap <minura.samaranayake@gmail.com>
2020-10-19 16:59:10 +0530

    added custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                        PermissionsMixin
 
+
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **extra_fields):   # Creates and saves a new User
+    def create_user(self, email, password=None, **extra_fields):
+        # Creates and saves a new User
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -12,15 +14,16 @@ class UserManager(BaseUserManager):
         user.save(using=self.db)
 
         return user
-    
+
     def create_superuser(self, email, password):
-        # Creates and saves new superuser 
+        # Creates and saves new superuser
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self.db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     # Custom user moedl that supports using email instead of username
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        email = "minura.samaranayake@gmail.com"
+        email = "test@gmail.com"
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -14,9 +15,8 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_uer_email_normalized(self):
-        email = 'minura.samaranayake@GMAIL.COM'
+        email = 'test@GMAIL.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
